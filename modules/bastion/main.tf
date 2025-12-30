d15f8dfc5e934adb2cdebf70d33a90624fc59cf1
Jakub Banaś <jakubbanas@Jakubs-MacBook-Air.local>
2025-12-30 21:56:36 +0100

    change path to ssh key

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_linux_virtual_machine" "bastion_vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("$HOME/.ssh/id_rsa.pub")
   }
 
   custom_data = filebase64("${path.module}/cloud-init.yaml")
