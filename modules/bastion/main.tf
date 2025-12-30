94ac07b748a3513a21b7ce4eddea6f4f83585605
nimbussigma <nimbussigma@gmail.com>
2025-12-30 22:08:12 +0100

    Add bastion VM module (private, no public IP) (#5)

---
+++ modules/bastion/main.tf
@@ -0,0 +1,60 @@
+resource "azurerm_network_security_group" "bastion_nsg" {
+  name                = "nsg-bastion-${var.name_prefix}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_network_interface" "bastion_nic" {
+  name                = "nic-bastion-${var.name_prefix}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_network_interface_security_group_association" "bastion_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.bastion_nic.id
+  network_security_group_id = azurerm_network_security_group.bastion_nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "bastion_vm" {
+  name                = "vm-bastion-${var.name_prefix}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = "Standard_B2s"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.bastion_nic.id
+  ]
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("${path.root}/../../assets/ssh/bastion_aks_p3.pub")
+  }
+
+  custom_data = filebase64("${path.module}/cloud-init.yaml")
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = var.tags
+}
---
+++ modules/bastion/outputs.tf
@@ -0,0 +1,7 @@
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.bastion_vm.id
+}
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.bastion_vm.name
+}
